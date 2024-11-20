c5ca89b5372e7e2de93d3d4578326a35df42f040
Aidan.Klug <aklug@luc.edu>
2024-11-20 12:24:07 -0600

    Added comments to main for clarity.

--- main.cpp
+++ main.cpp
@@ -15,18 +15,24 @@ int main(){
     input = "the quick brown fox jumps over the lazy dog.";
     cout << input << endl;
 
+    /* Ask inputProcessing to parse the input string into a freqArr ->
+     * 1. Vector of all the character in the string,
+     * 2. Vector of the frequency of occurrence of all character in the string.
+     */
     freqArr completedArrays = parseString(input);
 
     printArr(completedArrays);
 
     int size = completedArrays.chars.size();
 
+    // Build the huffman tree from the above freqArr struct
     struct MinHeapNode* root = buildHuffmanTree(completedArrays.chars, completedArrays.freqs, size);
 
     int intarr[MAX_TREE_HT], top = 0; 
 
 	printCodes(root, intarr, top); 
 
+    // Convert the input string into a bitstream using the above huffman tree.
     string bitstream = makeBitstream(root, input, completedArrays.freqs);
     std::cout << std::dec << bitstream << endl;
 
